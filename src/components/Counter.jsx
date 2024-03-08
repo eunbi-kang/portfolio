e66c7869615d241cbfa1083e0c7413c1cccbd084
eunbi-kang <ebzzang1905@naver.com>
2024-03-08 12:41:11 +0900

    state와 prop 이용하여 카운트 상태 끌어올리기

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,27 +1,22 @@
 import React, { useState } from "react";
 
-export default function Counter() {
+export default function Counter({ total, onClick }) {
   const [count, setCount] = useState(0);
   return (
-    <div className="container">
-      <div className="counter">
-        <span className="number">{count}</span>
-        <button
-          className="button"
-          onClick={() => {
-            setCount((prev)=> prev + 1);
-            // setCount((prev)=> prev + 1);
-            // setCount((prev)=> prev + 1);
-            // setCount((prev)=> prev + 1);
-            // setCount((prev)=> prev + 1);
-            console.log(count);
-          }
-        }
-          
-        >
-          Add +
-        </button>
-      </div>
+    <div className="counter">
+      <p className="number">
+        {count}
+        <span className="total">/{total}</span>
+      </p>
+      <button
+        className="button"
+        onClick={() => {
+          setCount((prev) => prev + 1);
+          onClick();
+        }}
+      >
+        Add+
+      </button>
     </div>
   );
 }
