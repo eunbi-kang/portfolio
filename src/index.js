f6a73b8868f177b092b8d17cb21debb8f557b3a9
eunbi-kang <ebzzang1905@naver.com>
2024-03-10 16:17:20 +0900

    index.js 컴포넌트 추가

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppProducts />
-    {/* <AppCounter />
-    <AppProfile /> */}
+    <AppCounter />
+    <AppProfile />
   </React.StrictMode>
 );
 
