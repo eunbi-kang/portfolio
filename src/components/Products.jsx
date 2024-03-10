d2e029b341750fc9737226f24cebd4312659c10f
eunbi-kang <ebzzang1905@naver.com>
2024-03-10 16:15:27 +0900

    useEffect 제대로 사용하여 판매제품, 세일제품 토글링 버튼 이용하여 표시해보기

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,18 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Products() {
-  const [count, setCount] = useState(0);
-  const [products, setProducts] = useState([]);
 
-  fetch("/public/data/products.json")
-    .then((res) => res.json())
+  const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
+  const handleChange =() => setChecked((prev)=> !prev);
+  // //cors
+
+  useEffect(()=> {
+    fetch(`data/${checked ? 'sale_': ''}products.json`, {
+      headers: {
+        Accept: "application/json"
+      }
+    })
+    .then((res)=> res.json()) 
     .then((data) => {
       console.log("🔥뜨끈한 데이터를 네트워크에서 받아옴");
       setProducts(data);
-      console.log(data);
     });
+    return () => {
+	    console.log('🧹깨끗하게 청소하는 일들을 합니다.')
+    }
+  }, [checked]);
+
   return (
     <>
+    <input type="checkbox" value={checked} onChange={handleChange} />
+    <label htmlFor='checkbox'>Show Only 🔥 Sale</label>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
@@ -23,7 +37,6 @@ export default function Products() {
           </li>
         ))}
       </ul>
-      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
     </>
   );
 }
